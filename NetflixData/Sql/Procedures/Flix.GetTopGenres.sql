e37fa807d58f6fd1448069becff430be61c18899
Mason Wittman <mawittma@ksu.edu>
2022-04-23 12:35:08 -0500

    GetTopGenres added

---
+++ NetflixData/Sql/Procedures/Flix.GetTopGenres.sql
@@ -0,0 +1,17 @@
+﻿CREATE OR ALTER PROCEDURE Flix.GetTopGenres
+@UserID INT
+AS
+WITH TopG(Genre, GenreCount, GenreID) AS
+	(
+		SELECT G.Genre, Count(*), GenreID
+		FROM Flix.UserShowLibrary USL 
+			INNER JOIN Flix.Show S ON USL.ShowID = S.ShowID
+			INNER JOIN Flix.ShowGenres SG ON SG.ShowID = S.ShowID
+			INNER JOIN Flix.Genre G ON G.GenreID = SG.GenreID
+		WHERE USL.UserID = @UserID
+		GROUP BY G.Genre
+	)
+SELECT TOP(5) G.Genre, G.GenreID 
+FROM Flix.TopG G
+ORDER BY G.GenreCount DESC
+GO
